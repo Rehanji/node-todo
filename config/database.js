ecafa40910e0bde5488f2f1c4f450316f4189285
Rehanji <35926731+Rehanji@users.noreply.github.com>
2018-04-19 19:41:46 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://Rehanji:Salehaji9911@ec2-54-164-4-45.compute-1.amazonaws.com:27017/dummyDB',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
